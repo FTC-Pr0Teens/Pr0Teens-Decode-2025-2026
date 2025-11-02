1d82f381720636dea1b069bef15b87ae14973485
Kirby Lau <kirbylau0@gmail.com>
2025-11-02 11:32:08 -0500

    feat(tested teleop)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/SampleTeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/SampleTeleOpMode.java
@@ -2,55 +2,57 @@ package org.firstinspires.ftc.teamcode.opmodes.tests;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.VoltageSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Hardware;
-
-import org.firstinspires.ftc.teamcode.subsystems.lift.LiftCommand;
 import org.firstinspires.ftc.teamcode.subsystems.mecanum.MecanumCommand;
-import org.firstinspires.ftc.teamcode.util.GoBildaPinpointDriver;
+import org.firstinspires.ftc.teamcode.subsystems.odometry.PinPointOdometrySubsystem;
 
 
 @TeleOp(name = "TeleopSample", group = "TeleOp")
 public class SampleTeleOpMode extends LinearOpMode {
 
     // opmodes should only own commands
-    private GoBildaPinpointDriver odo;
-
-    // Current pose estimates (in cm or degrees as appropriate)
     private MecanumCommand mecanumCommand;
-    private LiftCommand liftCommand;
-
     private ElapsedTime timer;
-
+    private Hardware hw;
     private ElapsedTime resetTimer;
 
-    private Hardware hw;
+    private static final double PUSHER_UP = 0.4;
+    private static final double PUSHER_DOWN = 1.0;
+    private static final long PUSHER_TIME = 500;
 
-    double heading;
-    enum ROBOT_STATE{
-        IDLE, SLOW
-    }
+    // --- Button edge detection ---
+    private boolean previousAState = false;
+    private boolean previousXState = false;
+    private boolean previousYState = false;
 
-    public ROBOT_STATE liftState = ROBOT_STATE.IDLE;
+    // --- Toggles/states ---
+    private boolean isIntakeMotorOn = false;
+    private boolean isOuttakeMotorOn = false;
+
+    // --- Pusher pulse state ---
+    private final ElapsedTime pusherTimer = new ElapsedTime();
+    private boolean isPusherUp = false;
+
 
     @Override
     public void runOpMode() throws InterruptedException {
         hw = Hardware.getInstance(hardwareMap);
         mecanumCommand = new MecanumCommand(hw);
-        liftCommand = new LiftCommand(hw);
-
-//        liftCommand = new LiftCommand(hw);
         resetTimer = new ElapsedTime();
+        hw.pusher.setPosition(PUSHER_DOWN);
+
         // Wait for start button to be pressed
         waitForStart();
 
         // Loop while OpMode is running
         while (opModeIsActive()) {
-            telemetry.addLine("chaewon");
-            heading = mecanumCommand.fieldOrientedMove(
-                    - gamepad1.left_stick_y,
+            mecanumCommand.processOdometry();
+            mecanumCommand.fieldOrientedMove(
+                    gamepad1.left_stick_y,
                     gamepad1.left_stick_x,
                     gamepad1.right_stick_x
             );
@@ -60,52 +62,52 @@ public class SampleTeleOpMode extends LinearOpMode {
             if (gamepad1.start){
                 mecanumCommand.resetPinPointOdometry();
             }
-            if(gamepad1.left_bumper) {
-                liftCommand.handleIntake();
+
+            // --- Intake toggle on A (edge) ---
+            boolean currentAState = gamepad1.a;
+            if (currentAState && !previousAState) {
+                isIntakeMotorOn = !isIntakeMotorOn;
+                hw.intake.setPower(isIntakeMotorOn ? 0.8 : 0.0);
+            }
+            previousAState = currentAState;
 
 
+            // --- Pusher pulse on Y (edge) ---
+            boolean currentYState = gamepad1.y;
+            if (currentYState && !previousYState) {
+                // Start pulse only if not already pulsing
+                if (!isPusherUp) {
+                    hw.pusher.setPosition(PUSHER_UP);
+                    pusherTimer.reset();
+                    isPusherUp = true;
+                }
             }
-            if (!gamepad1.left_bumper){
-                liftCommand.stopintake();
-            }
-            if(gamepad1.a){
-                liftCommand.turn();
-            }
-            if(gamepad1.b){
-                liftCommand.turn2();
-            }
-            if(gamepad1.x){
-                liftCommand.turn3();
-            }
-            if(gamepad1.right_bumper){
-                liftCommand.push();
-            }
-            if(gamepad1.dpad_up){
-                liftCommand.pull();
-            }
-           if(gamepad1.left_trigger > 0.1){
-                liftCommand.shoot();
-           }
-            if(gamepad1.right_trigger > 0.1){
-                liftCommand.shootstop();
+            previousYState = currentYState;
+
+            // Pusher
+            if (isPusherUp && pusherTimer.milliseconds() >= PUSHER_TIME) {
+                hw.pusher.setPosition(PUSHER_DOWN);
+                isPusherUp = false;
             }
 
-
+            // Outtake
+            boolean currentXState = gamepad1.x;
+            if (currentXState && !previousXState) {
+                isOuttakeMotorOn = !isOuttakeMotorOn;
+                hw.shooter.setPower(isOuttakeMotorOn ? 1.0 : 0.0);
+            }
+            previousXState = currentXState;
 
         }
 
     }
-    public void processTelemetry() {
+    public void processTelemetry(){
         //add telemetry messages here
-        mecanumCommand.deadReckoning();
-
-        telemetry.addData("x: ", mecanumCommand.getOdoX());
-        telemetry.addData("y: ", mecanumCommand.getOdoY());
-        telemetry.addData("heading: ", heading);
+        telemetry.addData("resetTimer: ",  resetTimer.milliseconds());
+        telemetry.addLine("---------------------------------");
+        telemetry.addData("X", mecanumCommand.getX());
+        telemetry.addData("Y", mecanumCommand.getY());
+        telemetry.addData("Pusher ON", isPusherUp);
         telemetry.update();
     }
 }
-
-
-
-
