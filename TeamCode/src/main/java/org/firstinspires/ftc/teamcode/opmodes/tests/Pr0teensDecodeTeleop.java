530f20036aa98cd53ed25bd7481f14b7c4849f5d
Kirby Lau <kirbylau0@gmail.com>
2025-11-29 00:16:59 -0500

    feat: New Tele

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/Pr0teensDecodeTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/Pr0teensDecodeTeleop.java
@@ -14,12 +14,15 @@ import org.firstinspires.ftc.teamcode.subsystems.mecanum.MecanumCommand;
 import org.firstinspires.ftc.teamcode.subsystems.outtake.OuttakeCommand;
 import org.firstinspires.ftc.teamcode.subsystems.outtake.OuttakeSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.sorting.SortingSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.cameras.LogitechSubsystem;
 
 @TeleOp(name = "Pr0teens Comp Tele", group = "TeleOp")
 public class Pr0teensDecodeTeleop extends LinearOpMode {
 
     private MecanumCommand mecanumCommand;
     private OuttakeCommand outtakeCommand;
+    private LogitechSubsystem logitechsub;
+
     private Hardware hw;
 
     // --- Button Variables ---
@@ -46,19 +49,31 @@ public class Pr0teensDecodeTeleop extends LinearOpMode {
     private static final double SORTER_FIRST_POS = 0.0;
     private static final double SORTER_SECOND_POS = 0.45;
     private static final double SORTER_THIRD_POS = 0.90;
+    private String ALLIANCE = "blue";
+    private boolean autoAimState = false;
+    private boolean previousAimButton = false;
 
     @Override
     public void runOpMode() throws InterruptedException {
         hw = Hardware.getInstance(hardwareMap);
         mecanumCommand = new MecanumCommand(hw);
         outtakeCommand = new OuttakeCommand(hw);
+        logitechsub = new LogitechSubsystem(hw, ALLIANCE);
+
 
         hw.intake.setDirection(DcMotorSimple.Direction.REVERSE);
         hw.shooter.setDirection(DcMotorSimple.Direction.REVERSE);
         hw.pusher1.setDirection(Servo.Direction.REVERSE);
         hw.shooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
         outtakeCommand.setMaxRPM(4000);
         while (opModeInInit()) {
+            if (gamepad1.b) {
+                ALLIANCE = "red";
+            }
+            if (gamepad1.x) {
+                ALLIANCE = "blue";
+            }
             hw.pusher.setPosition(0);
             hw.pusher1.setPosition(0);
             hw.sorter.setPosition(0);
@@ -70,6 +85,12 @@ public class Pr0teensDecodeTeleop extends LinearOpMode {
         // Loop while OpMode is running
         while (opModeIsActive()) {
             processTelemetry();
+            if (isOuttakeMotorOn){
+                outtakeCommand.spinup();
+            } else {
+                outtakeCommand.stopShooter();
+            }
+
             mecanumCommand.normalMove(
                     -gamepad1.left_stick_y,
                     gamepad1.left_stick_x,
@@ -77,7 +98,7 @@ public class Pr0teensDecodeTeleop extends LinearOpMode {
             );
 
             //Intake backwards
-            boolean currentLBumpState = gamepad1.left_bumper;
+            boolean currentLBumpState = gamepad2.left_bumper;
             if (currentLBumpState && !previousLBumpState) {
                 hw.intake.setDirection(DcMotorSimple.Direction.FORWARD);
                 isIntakeMotorOn = !isIntakeMotorOn;
@@ -86,7 +107,7 @@ public class Pr0teensDecodeTeleop extends LinearOpMode {
             previousLBumpState = currentLBumpState;
 
             // --- Intake toggle on A ---
-            boolean currentAState = gamepad1.a;
+            boolean currentAState = gamepad2.a;
             if (currentAState && !previousAState) {
                 hw.intake.setDirection(DcMotorSimple.Direction.REVERSE);
                 isIntakeMotorOn = !isIntakeMotorOn;
@@ -95,7 +116,7 @@ public class Pr0teensDecodeTeleop extends LinearOpMode {
             previousAState = currentAState;
 
             // --- Pusher up on Y  ---
-            boolean currentYState = gamepad1.y;
+            boolean currentYState = gamepad2.y;
             if (currentYState && !previousYState) {
                 // Start pulse only if not already pulsing
                 if (!isPusherUp) {
@@ -114,14 +135,13 @@ public class Pr0teensDecodeTeleop extends LinearOpMode {
                 isPusherUp = false;
             }
 
-            boolean currentXState = gamepad1.x;
-
+            boolean currentXState = gamepad2.x;
             if (currentXState && !previousXState) {
                 isOuttakeMotorOn = !isOuttakeMotorOn;
             }
             previousXState = currentXState;
 
-            if (gamepad1.b && sorterTimer.milliseconds() > 800 && !isPusherUp) {
+            if (gamepad2.b && sorterTimer.milliseconds() > 800 && !isPusherUp) {
                 sorterTimer.reset();
                 if (sorterpos == 0) {
                     hw.sorter.setPosition(SORTER_FIRST_POS);//60 degrees
@@ -132,10 +152,37 @@ public class Pr0teensDecodeTeleop extends LinearOpMode {
                 }
                 sorterpos = (sorterpos + 1) % 3;
             }
+
+            if (gamepad2.right_bumper && !previousAimButton) {
+                autoAimState = !autoAimState;   // toggle on *edge* of button press
+            }
+            previousAimButton = gamepad2.right_bumper;
+
+            if (autoAimState) {
+                if (logitechsub.targetApril() > 5) {
+                    mecanumCommand.pivot(0.2);
+                } else if (logitechsub.targetApril() < -5) {
+                    mecanumCommand.pivot(-0.2);
+                } else {
+                    mecanumCommand.pivot(0);
+                }
+            }
+
+            if (isOuttakeMotorOn) {
+                if (logitechsub.distance() >= 100) {
+                    outtakeCommand.setMaxRPM(3500);
+                } else if (logitechsub.distance() <= 90 && logitechsub.distance() >= 35) {
+                    outtakeCommand.setMaxRPM(2800);
+                } else if (logitechsub.distance() <= 35) {
+                    outtakeCommand.setMaxRPM(2300);
+                }
+            }
         }
     }
     public void processTelemetry() {
         telemetry.addData("RPM", hw.shooter.getVelocity() * 60.0 / 28.0);
+        telemetry.addData("x", logitechsub.targetApril());
+        telemetry.addData("y", logitechsub.distance());
         telemetry.update();
     }
 }
